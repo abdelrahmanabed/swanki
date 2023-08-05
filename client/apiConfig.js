868baa9cbe986c8885b5198631a1fbaeed6a2aa7
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2023-08-05 08:30:31 +0300

    chngg
    swan

--- client/apiConfig.js
+++
@@ -1,5 +0,0 @@
-import getConfig from 'next/config';
-export function getApiBaseUrl() {
-    const { publicRuntimeConfig } = getConfig();
-    return publicRuntimeConfig.API_BASE_URL || 'http://localhost:5000';
-  }
--- client/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // ...
-    experiments: {
-      topLevelAwait: true,
-    },
-  };
