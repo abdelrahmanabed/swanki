b9f8711c5002cfe88957d219db2e8ee93ec8c556
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2023-08-06 03:34:28 +0300

    1
    2

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.json())
 app.use(cors())
 app.use(function (req,res,next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE")
-  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Content-Type, Accept, Authorization")  
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Content-Type, Accept")  
 })
 const dotenv = require('dotenv');
 dotenv.config();
