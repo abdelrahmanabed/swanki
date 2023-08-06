68efd79be0babf5d24ca44490e75eb4cb24c456d
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2023-08-06 04:09:46 +0300

    yess
    yes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(cors())
 app.use(function (req,res,next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Content-Type, Accept")  
+  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Content-Type, Accept") 
+  next(); 
 })
 const dotenv = require('dotenv');
 dotenv.config();
